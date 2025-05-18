e287db0db7d419b351c8be12c5bf85bf7f01cbae
Bradley Latreille <bradltr95@gmail.com>
2025-05-17 22:53:49 -0400

    task: write passing test cases for each scenario
    - Write a testcase to check if the ship is hit.
    - Write a testcase to check if the ship was missed.
    - Write a testcase to check if the ship was killed.

--- src/test/BattleshipTest.java
+++ src/test/BattleshipTest.java
@@ -5,22 +5,42 @@ import main.Battleship;
 class BattleshipTest {
     public static void main(String[] args) {
         System.out.println("Test Case 1: " + checkMissTest());
+        System.out.println("Test Case 2: " + checkHitTest());
+        System.out.println("Test Case 3: " + checkKillTest());
     }
 
     // Complete writing this test case
-    public static void checkHitTest() {
+    public static String checkHitTest() {
+        Battleship ship = new Battleship();
+        ship.setLocation(4); // Will set the location cells to 4, 5, 6
+        String hitResult = ship.checkHit(4);
 
+        String testResult = "failed";
+        if(hitResult.equals("hit")) {
+            testResult = "passed";
+        }
+        return testResult;
     }
 
     // Complete writing this test case
-    public static void checkKillTest() {
-
+    public static String checkKillTest() {
+        Battleship ship = new Battleship();
+        ship.setLocation(4); // Will set the location cells to 4, 5, 6
+        ship.checkHit(4);
+        ship.checkHit(5);
+        // We only need to capture the last hit to see if we get a "kill"
+        String hitResult = ship.checkHit(6);
+        String testResult = "failed";
+        if(hitResult.equals("kill")) {
+            testResult = "passed";
+        }
+        return testResult;
     }
 
     public static String checkMissTest() {
         Battleship ship = new Battleship();
         ship.setLocation(4); // Will set the location cells to 4, 5, 6
-        String hitResult = ship.checkHit(3); // Since 3 is not part of the location cells this should return "miss"
+        String hitResult = ship.checkHit(3);
 
         String testResult = "failed";
         if(hitResult.equals("miss")) {
